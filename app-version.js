29b59677acf185e87a7ce3632b15605b6d6b4603
rayvidal1 <rayvidalrodrigues@gmail.com>
2026-03-21 08:46:31 -0300

    Refresh PWA cache version

--- app-version.js
+++ app-version.js
@@ -1,3 +1,3 @@
 (function (globalScope) {
-  globalScope.RENOVO_APP_VERSION = "2026-03-17-d";
+  globalScope.RENOVO_APP_VERSION = "2026-03-21-a";
 })(typeof self !== "undefined" ? self : globalThis);

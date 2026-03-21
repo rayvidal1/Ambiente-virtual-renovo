c2de2a55ecd21717d32de994e78dd33418974857
rayvidal1 <rayvidalrodrigues@gmail.com>
2026-03-21 09:01:02 -0300

    Soften login card background

--- app-version.js
+++ app-version.js
@@ -1,3 +1,3 @@
 (function (globalScope) {
-  globalScope.RENOVO_APP_VERSION = "2026-03-21-a";
+  globalScope.RENOVO_APP_VERSION = "2026-03-21-b";
 })(typeof self !== "undefined" ? self : globalThis);

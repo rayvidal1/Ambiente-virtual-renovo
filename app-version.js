70efd4530b6ea24372a2a9b0792c800be125d14e
rayvidal1 <rayvidalrodrigues@gmail.com>
2026-03-22 05:46:45 -0300

    Deploy 2026-03-22-c: sync SW update na pagina instalar

--- app-version.js
+++ app-version.js
@@ -1,3 +1,3 @@
 (function (globalScope) {
-  globalScope.RENOVO_APP_VERSION = "2026-03-22-b";
+  globalScope.RENOVO_APP_VERSION = "2026-03-22-c";
 })(typeof self !== "undefined" ? self : globalThis);

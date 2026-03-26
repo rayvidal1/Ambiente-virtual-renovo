9f875ac2d6be552e8d44d1fcc5c42a9df74869b8
rayvidal1 <rayvidalrodrigues@gmail.com>
2026-03-26 09:39:10 -0300

    Deploy 2026-03-26-b: corrige quebra de linha no botao Nao

--- app-version.js
+++ app-version.js
@@ -1,3 +1,3 @@
 (function (globalScope) {
-  globalScope.RENOVO_APP_VERSION = "2026-03-26-a";
+  globalScope.RENOVO_APP_VERSION = "2026-03-26-b";
 })(typeof self !== "undefined" ? self : globalThis);

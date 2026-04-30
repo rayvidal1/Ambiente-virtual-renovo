cd4c8dd57a74d668d2b1e004e0a78e4ade979f16
rayvidal1 <rayvidalrodrigues@gmail.com>
2026-04-30 08:31:27 -0300

    Redirect legacy app shell to Renovo Plus

--- app-version.js
+++ app-version.js
@@ -1,3 +1,3 @@
 (function (globalScope) {
-  globalScope.RENOVO_APP_VERSION = "2026-04-30-renovo-plus-default";
+  globalScope.RENOVO_APP_VERSION = "2026-04-30-renovo-plus-sw-redirect";
 })(typeof self !== "undefined" ? self : globalThis);
